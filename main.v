cd978ea9189a57efe57b6684a313acb0d9ebdba2
nyaascii <contrib@nyaa.science>
2020-06-29 19:53:15 +0200

    feat: Make GitHub username and domain name a const

--- main.v
+++ main.v
@@ -3,10 +3,15 @@ module main
 import picoev
 import picohttpparser
 
+const (
+    github_account = "nyaascii"
+    module_domain  = "pkg.nyaa.science"
+)
+
 [inline]
 fn vanity_template(path string) string {
     escaped_path := path.replace("<", "&lt;").replace(">", "&qt;").replace('"', "&quot;")
-    return '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="go-import" content="ddd.codes$escaped_path git github.com/ddd-org/$escaped_path"><meta name="go-source" content="github.com/ddd-org$escaped_path _ github.com/ddd-org$escaped_path/tree/{/dir} github.com/ddd-org$escaped_path/blob/\{/dir}/\{file}#L\{line}"></head></html>'
+    return '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="go-import" content="$module_domain$escaped_path git github.com/$github_account/$escaped_path"><meta name="go-source" content="github.com/$github_account$escaped_path _ github.com/$github_account$escaped_path/tree/{/dir} github.com/$github_account$escaped_path/blob/\{/dir}/\{file}#L\{line}"></head></html>'
 }
 
 fn callback(req picohttpparser.Request, mut res picohttpparser.Response) {

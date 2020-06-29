1df5b6e3420029e6b53eb664537359fb8a8c813a
nyaascii <contrib@nyaa.science>
2020-06-30 00:07:17 +0200

    feat: Configurable port and some additional HTML structure

--- main.v
+++ main.v
@@ -6,12 +6,13 @@ import picohttpparser
 const (
     github_account = "nyaascii"
     module_domain  = "pkg.nyaa.science"
+    port           = 3001
 )
 
 [inline]
 fn vanity_template(path string) string {
     escaped_path := path.replace("<", "&lt;").replace(">", "&qt;").replace('"', "&quot;")
-    return '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="go-import" content="$module_domain$escaped_path git github.com/$github_account$escaped_path"><meta name="go-source" content="github.com/$github_account$escaped_path _ github.com/$github_account$escaped_path/tree/{/dir} github.com/$github_account$escaped_path/blob/{/dir}/{file}#L{line}"></head></html>'
+    return '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>${escaped_path[1..]}</title><meta name="go-import" content="$module_domain$escaped_path git github.com/$github_account$escaped_path"><meta name="go-source" content="github.com/$github_account$escaped_path _ github.com/$github_account$escaped_path/tree/{/dir} github.com/$github_account$escaped_path/blob/{/dir}/{file}#L{line}"></head><body><a href="https://github.com/$github_account$escaped_path" style="font-family: monospace">$module_domain$escaped_path</a></body></html>'
 }
 
 fn callback(req picohttpparser.Request, mut res picohttpparser.Response) {
@@ -27,6 +28,6 @@ fn callback(req picohttpparser.Request, mut res picohttpparser.Response) {
 }
 
 fn main() {
-    println('Listening at :3000')
-    picoev.new(3000, &callback).serve()
+    println('Listening at :$port')
+    picoev.new(port, &callback).serve()
 }

786f6744053f84ce52c8b97b4f4e6abfbc44fddc
nyaascii <contrib@nyaa.science>
2020-06-29 19:57:15 +0200

    fix: Remove unnecessary escape characters

--- main.v
+++ main.v
@@ -11,7 +11,7 @@ const (
 [inline]
 fn vanity_template(path string) string {
     escaped_path := path.replace("<", "&lt;").replace(">", "&qt;").replace('"', "&quot;")
-    return '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="go-import" content="$module_domain$escaped_path git github.com/$github_account$escaped_path"><meta name="go-source" content="github.com/$github_account$escaped_path _ github.com/$github_account$escaped_path/tree/{/dir} github.com/$github_account$escaped_path/blob/\{/dir}/\{file}#L\{line}"></head></html>'
+    return '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="go-import" content="$module_domain$escaped_path git github.com/$github_account$escaped_path"><meta name="go-source" content="github.com/$github_account$escaped_path _ github.com/$github_account$escaped_path/tree/{/dir} github.com/$github_account$escaped_path/blob/{/dir}/{file}#L{line}"></head></html>'
 }
 
 fn callback(req picohttpparser.Request, mut res picohttpparser.Response) {
